ab0bac0e62592d687f72bf981402b3fc8860e4ae
joachimschmidt557 <joachim.schmidt557@outlook.com>
2021-03-16 11:06:39 +0100

    Bump version to 1.0.0

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ with pkgs;
 
 buildGoPackage rec {
   pname = "timewsync-server";
-  version = "0.0.1";
+  version = "1.0.0";
   goPackagePath = "git.rwth-aachen.de/computer-aided-synthetic-biology/bachelorpraktika/2020-67-timewarrior-sync/timew-sync-server";
   src = ./.;
   goDeps = ./deps.nix;
--- main.go
+++ main.go
@@ -73,7 +73,7 @@ func main() {
 	default:
 		flag.Parse()
 		if versionFlag {
-			_, _ = fmt.Fprintf(os.Stderr, "timewarrior sync server version %v\n", "unreleased")
+			_, _ = fmt.Fprintf(os.Stderr, "timewarrior sync server version %v\n", "1.0.0")
 			os.Exit(0)
 		} else {
 			log.Fatal("Use commands start, add-user or add-key")
