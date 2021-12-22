27da7a4c4a676456f27aa4eb3de921ed933417cc
joachimschmidt557 <joachim.schmidt557@outlook.com>
2021-12-22 22:55:12 +0100

    Change to nix flakes
    Nix flakes will enable more reproducible development environments

--- default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-with pkgs;
-
-buildGoModule rec {
-  pname = "timewsync-server";
-  version = "1.0.0";
-  src = ./.;
-  vendorSha256 = "0wbd4cpswgbr839sk8qwly8gjq4lqmq448m624akll192mzm9wj7";
-}
